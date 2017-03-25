949708ca67b3ea9df92ff00d2546ba3de59cb1f4
Sophelipe <526577718@qq.com>
2017-03-25 11:07:25 +0800

    load data by loop

--- test.py
+++ test.py
@@ -3,12 +3,15 @@ import caffe
 import cv2
 import numpy as np
 from dataLayer import DataLoader
+import platform
+from itertools import islice
+
 model = r'../../models/bvlc_googlenet/bvlc_googlenet.caffemodel'
 deploy = r'../../models/bvlc_googlenet/deploy.prototxt'
 
-caffe.set_mode_gpu()
-caffe.set_device(0)
-net = caffe.Net(deploy, model, caffe.TEST)
+# caffe.set_mode_gpu()
+# caffe.set_device(0)
+# net = caffe.Net(deploy, model, caffe.TEST)
 dataloader = DataLoader({'state':'test'})
 batch_size = 10;
 shoplist = []
@@ -16,13 +19,14 @@ customlist = []
 for i in range(batch_size):
 	img_shop, img_custom, label = dataloader.load_data()
 	customlist.append(img_custom)
+	print customlist
 
 customlist = np.array(customlist)
 
-net.blobs['data'].reshape(*customlist.shape)
-net.blobs['data'].data[...] = customlist
-print len(customlist)
-net.forward()
+# net.blobs['data'].reshape(*customlist.shape)
+# net.blobs['data'].data[...] = customlist
+# print len(customlist)
+# net.forward()
 
-print net.blobs['pool5/7x7_s1'].data[...].shape
-print net.blobs['pool5/7x7_s1'].data[0,...]
+# print net.blobs['pool5/7x7_s1'].data[...].shape
+# print net.blobs['pool5/7x7_s1'].data[0,...]
