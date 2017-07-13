a68b26d51b04e40972ccc7e969eaadd1a3d3ee5a
Sopheliple <526577718@qq.com>
2017-07-13 22:02:51 +0800

    add

--- train_mnist_siamese.sh
+++ train_mnist_siamese.sh
@@ -2,5 +2,7 @@
 set -e
 
 TOOLS=./build_py/tools
+GLOG_logtostderr=0 GLOG_log_dir=examples/tes/log/train2/ \
+$TOOLS/caffe train --solver=examples/tes/train_siamese_solver.prototxt \
+--snapshot=examples/tes/train1/train2_iter_160836.solverstate
 
-$TOOLS/caffe train --solver=examples/tes/train_siamese_solver.prototxt $@
